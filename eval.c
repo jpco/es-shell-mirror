9fac083b4fc628c75eaa68d91f809e0f931aedb0
jpco <mail@jpco.io>
2017-12-06 17:50:28 -0800

    fix a panic

--- eval.c
+++ eval.c
@@ -286,7 +286,8 @@ top:
     switch (tree->kind) {
 
     case nConcat: case nList: case nQword: case nVar: case nVarsub:
-    case nWord: case nThunk: case nLambda: case nCall: case nPrim: {
+    case nWord: case nThunk: case nLambda: case nCall: case nPrim: 
+    case nArith: {
         List *list;
         Ref(Binding *, bp, binding);
         list = glom(tree, binding, TRUE);
