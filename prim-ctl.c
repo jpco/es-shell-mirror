d3cc20380733fbb8bdcd879e34795cda6c21c692
jpco <mail@jpco.io>
2018-11-08 01:05:16 -0800

    Fix segfaults on exceptions with GCALWAYS

--- prim-ctl.c
+++ prim-ctl.c
@@ -68,8 +68,8 @@ PRIM(catch) {
             blocksignals();
             ExceptionHandler
                 result
-                  = eval(mklist(mkstr("$&noreturn"),
-                            mklist(lp->term, frombody)),
+                  = prim("noreturn",
+                     mklist(lp->term, frombody),
                      NULL,
                      evalflags);
                 unblocksignals();
